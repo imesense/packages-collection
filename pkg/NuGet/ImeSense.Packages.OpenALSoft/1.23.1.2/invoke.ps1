c8feead13e85ac43aa3c4d24b806927fca58077e
acidicMercury8 <acidicmercury8@outlook.com>
2023-10-27 13:19:12 +0300

    Rewrite build recipe for OpenAL Soft

---
+++ pkg/NuGet/ImeSense.Packages.OpenALSoft/1.23.1.2/invoke.ps1
@@ -0,0 +1,62 @@
+$Source      = "https://github.com/kcat/openal-soft.git"
+$Branch      = "1.23.1"
+$Destination = "dep/kcat/openal-soft/$Branch"
+
+$Root   = "../../../.."
+$Output = "out"
+
+function Invoke-Get {
+    if (!(Test-Path -Path "$Destination" -ErrorAction SilentlyContinue)) {
+        git clone --branch $Branch --depth 1 $Source $Destination
+    }
+}
+
+function Invoke-Build {
+    $installer = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $path      = & $installer -latest -prerelease -property installationPath
+
+    pushd "$path\Common7\Tools"
+    cmd /c "VsDevCmd.bat&set" |
+    foreach {
+        if ($_ -Match "=") {
+            $v = $_.Split("=", 2)
+            Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
+        }
+    }
+    popd
+    Write-Host "Visual Studio 2022 Command Prompt" -ForegroundColor Yellow
+
+    cmake `
+        -S $Destination `
+        -B $Destination/build/Win32 `
+        -G "Visual Studio 17 2022" `
+        -A Win32 `
+        -T host=x64 `
+        -DALSOFT_REQUIRE_WINMM=ON `
+        -DALSOFT_REQUIRE_DSOUND=ON `
+        -DALSOFT_REQUIRE_WASAPI=ON
+    cmake `
+        -S $Destination `
+        -B $Destination/build/x64 `
+        -G "Visual Studio 17 2022" `
+        -A x64 `
+        -T host=x64 `
+        -DALSOFT_REQUIRE_WINMM=ON `
+        -DALSOFT_REQUIRE_DSOUND=ON `
+        -DALSOFT_REQUIRE_WASAPI=ON
+
+    cmake --build $Destination/build/Win32 --config Debug
+    cmake --build $Destination/build/Win32 --config Release
+    cmake --build $Destination/build/x64 --config Debug
+    cmake --build $Destination/build/x64 --config Release
+}
+
+function Invoke-Pack {
+    nuget pack $PSScriptRoot\ImeSense.Packages.OpenALSoft.nuspec -OutputDirectory $Output
+}
+
+function Invoke-Actions {
+    Invoke-Get
+    Invoke-Build
+    Invoke-Pack
+}
--- pkg/openal-soft/get.bat
+++
@@ -1,2 +0,0 @@
-:: Get sources
-git clone --branch 1.23.1 --depth 1 https://github.com/kcat/openal-soft.git ../../dep/openal-soft/v1.23.1
