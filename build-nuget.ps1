7472a0363cf7a2e560cc1986be2e0677dbbc242a
acidicMercury8 <acidicmercury8@outlook.com>
2023-12-10 13:51:43 +0300

    Add build recipe for FreeType 2.13.2 and FreeType dated 2023.12.07
    Implements request #7

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,5 +1,6 @@
 # Build release packages
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeImage.WinMerge -Version 3.18.0.20230819
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType           -Version 2.13.2
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibOgg             -Version 1.3.5.5
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibTheora          -Version 1.1.1.4
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibVorbis          -Version 1.3.7.5
@@ -8,6 +9,7 @@
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.Zlib               -Version 1.2.13.3
 
 # Build pre-release packages
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType   -Version 2023.12.7-open
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.LuaJIT     -Version 2.1.0-beta3
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.Marl       -Version 2023.11.29-open
 . .\build.ps1 -Type NuGet -Name ImeSense.Packages.OpenALSoft -Version 2023.11.8-open
---
+++ pkg/NuGet/ImeSense.Packages.FreeType/2.13.2/invoke.ps1
@@ -0,0 +1,68 @@
+$Source      = "https://gitlab.freedesktop.org/freetype/freetype.git"
+$Branch      = "VER-2-13-2"
+$Commit      = "920c5502cc3ddda88f6c7d85ee834ac611bb11cc"
+$Destination = "dep/freetype/freetype/$Branch"
+
+$Root   = "../../../.."
+$Output = "out"
+
+function Invoke-Get {
+    if (!(Test-Path -Path "$Destination" -ErrorAction SilentlyContinue)) {
+        git clone --branch $Branch --depth 1 --recurse-submodules $Source $Destination
+    }
+}
+
+function Invoke-Patch {
+    Set-Location $Destination
+    git reset --hard $Commit
+    Set-Location $Root
+
+    Copy-Item -Path $Destination/README -Destination $Destination/README.md
+}
+
+function Invoke-Build {
+    $installer = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
+    $path      = & $installer -latest -prerelease -property installationPath
+
+    Push-Location "$path\Common7\Tools"
+    cmd /c "VsDevCmd.bat&set" |
+    ForEach-Object {
+        if ($_ -Match "=") {
+            $v = $_.Split("=", 2)
+            Set-Item -Force -Path "ENV:\$($v[0])" -Value "$($v[1])"
+        }
+    }
+    Pop-Location
+    Write-Host "Visual Studio 2022 Command Prompt" -ForegroundColor Yellow
+
+    cmake `
+        -S $Destination `
+        -B $Destination/build/Win32 `
+        -G "Visual Studio 17 2022" `
+        -A Win32 `
+        -T host=x64 `
+        -D BUILD_SHARED_LIBS=TRUE
+    cmake `
+        -S $Destination `
+        -B $Destination/build/x64 `
+        -G "Visual Studio 17 2022" `
+        -A x64 `
+        -T host=x64 `
+        -D BUILD_SHARED_LIBS=TRUE
+
+    cmake --build $Destination/build/Win32 --config Debug
+    cmake --build $Destination/build/Win32 --config Release
+    cmake --build $Destination/build/x64 --config Debug
+    cmake --build $Destination/build/x64 --config Release
+}
+
+function Invoke-Pack {
+    nuget pack $PSScriptRoot\ImeSense.Packages.FreeType.nuspec -OutputDirectory $Output
+}
+
+function Invoke-Actions {
+    Invoke-Get
+    Invoke-Patch
+    Invoke-Build
+    Invoke-Pack
+}
