88c0a3b1d7cc5a0d0df77937c76f47eda2dd7f95
acidicMercury8 <acidicmercury8@outlook.com>
2024-01-19 22:50:54 +0300

    Replace build recipe for FreeImage by WinMerge dated 2023.8.19

--- build-nuget.ps1
+++ build-nuget.ps1
@@ -1,19 +1,19 @@
 # Build release packages
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeImage.WinMerge -Version 3.18.0.20230819
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType           -Version 2.13.2
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibOgg             -Version 1.3.5.5
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibTheora          -Version 1.1.1.4
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibVorbis          -Version 1.3.7.5
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.OpenALSoft         -Version 1.23.1.2
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Optick             -Version 1.4.0
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Sdl                -Version 2.28.5
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.WxWidgets.Vcpkg    -Version 3.2.4
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Zlib               -Version 1.2.13.3
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType        -Version 2.13.2
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibOgg          -Version 1.3.5.5
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibTheora       -Version 1.1.1.4
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LibVorbis       -Version 1.3.7.5
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.OpenALSoft      -Version 1.23.1.2
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Optick          -Version 1.4.0
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Sdl             -Version 2.28.5
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.WxWidgets.Vcpkg -Version 3.2.4
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Zlib            -Version 1.2.13.3
 
 # Build pre-release packages
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType   -Version 2023.12.7-open
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LuaJIT     -Version 2.1.0-beta3
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Marl       -Version 2023.11.29-open
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.OpenALSoft -Version 2023.11.8-open
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Optick     -Version 2022.7.8-open
-. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Sdl        -Version 2023.12.9-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeImage.WinMerge -Version 2023.8.19-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.FreeType           -Version 2023.12.7-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.LuaJIT             -Version 2.1.0-beta3
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Marl               -Version 2023.11.29-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.OpenALSoft         -Version 2023.11.8-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Optick             -Version 2022.7.8-open
+. .\build.ps1 -Type NuGet -Name ImeSense.Packages.Sdl                -Version 2023.12.9-open
