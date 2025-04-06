5a6de2cc3f2c05092ee6f4b70d736c66340f7ad8
acidicMercury8 <acidicmercury8@outlook.com>
2025-04-06 23:01:25 +0300

    Add function for calculating timestamp

--- src/Common/Utilities.lua
+++ src/Common/Utilities.lua
@@ -25,4 +25,28 @@ function module.Contains(source, substring)
     return result
 end
 
+function module.GetTimestamp(date)
+    local year, month, day, hour, min, sec =
+        date:match("(%d+)-(%d+)-(%d+) (%d+):(%d+):(%d+)")
+    year = tonumber(year)
+    month = tonumber(month)
+    day = tonumber(day)
+    hour = tonumber(hour)
+    min = tonumber(min)
+    sec = tonumber(sec)
+
+    local timestamp = os.time({
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        year = year,
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        month = month,
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        day = day,
+        hour = hour,
+        min = min,
+        sec = sec
+    })
+    return timestamp
+end
+
 return module
