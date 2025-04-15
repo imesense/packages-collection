3496419665d523bceb18fe3e193b44eedc51bf8d
acidicMercury8 <acidicmercury8@outlook.com>
2025-04-15 04:19:01 +0300

    Add Lua wrapper for macOS

---
+++ run-script.zsh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+# Set environment variables
+export PATH=./bin:$PATH
+export LUA_PATH="./share/lua/5.1/?.lua;./share/lua/5.1/?/init.lua;./?.lua;./?/init.lua"
+export LUA_CPATH="./lib/lua/5.1/?.dylib;./?.dylib"
+
+# Check path to script in arguments
+if [ $# -eq 0 ]
+then
+    echo -e "\033[31mNo script is specified to run\033[0m"
+    exit 1
+fi
+
+# Get path to script from arguments
+scriptToRun=$1
+
+# Run script
+./bin/lua "$scriptToRun" "${@:2}"
